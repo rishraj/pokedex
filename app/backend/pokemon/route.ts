76d7d3cc2d21f6e39197b5da099dd30aff526b27
Rishav Raj <rishav.raj2309@gmail.com>
2024-11-09 11:00:48 +0530

    migration from SQLite to PostgreSQL to support Vercel

--- app/backend/pokemon/route.ts
+++ app/backend/pokemon/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+/* import { PrismaClient } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient()
@@ -26,4 +26,4 @@ export async function GET(request: NextRequest) {
       { status: 500}
     );
   }
-}
+} */
